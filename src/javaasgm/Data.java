24ab1d2b09c592728ea84deec93b6889c7f1d77b
User <User@DESKTOP-8NKD8GK>
2024-10-19 02:02:50 +0800

    add command line

--- src/javaasgm/Data.java
+++ src/javaasgm/Data.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/*We put our data read/write here*/
 package javaasgm;
 
 /**
